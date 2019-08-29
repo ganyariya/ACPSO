977c3d795e6eead622f4b6eccbdf46bf07c23d06
ganariya <willwill1997willwill@yahoo.co.jp>
2019-08-29 14:26:08 +0900

    描画にミスがあったので、修正

--- mypso.py
+++ mypso.py
@@ -10,10 +10,59 @@ import random
 
 def objective_function(position):
     t1 = 20
-    t2 = -20 * np.exp(-0.2 * np.sqrt(1.0 / len(position) * np.sum(position ** 2)))
+    t2 = -20 * np.exp(-0.2 * np.sqrt(1.0 / len(position) * np.sum(position ** 2, axis=0)))
     t3 = np.e
-    t4 = -np.exp(1.0 / len(position) * np.sum(np.cos(2 * np.pi * position)))
+    t4 = -np.exp(1.0 / len(position) * np.sum(np.cos(2 * np.pi * position), axis=0))
     return t1 + t2 + t3 + t4
+
+
+# 描画のための初期化
+def init_plot(xy_min, xy_max):
+    matplot_x = np.arange(xy_min, xy_max, 1.0)
+    matplot_y = np.arange(xy_min, xy_max, 1.0)
+
+    matplot_mesh_X, matplot_mesh_Y = np.meshgrid(matplot_x, matplot_y)
+
+    Z = []
+    for i in range(len(matplot_mesh_X)):
+        z = []
+        for j in range(len(matplot_mesh_X[0])):
+            result = objective_function(np.array([matplot_mesh_X[i][j], matplot_mesh_Y[i][j]]))
+            z.append(result)
+        Z.append(z)
+    Z = np.array(Z)
+
+    plt.ion()
+    fig = plt.figure()
+    axes = Axes3D(fig)
+
+    axes.set_xlabel('x')
+    axes.set_ylabel('y')
+    axes.set_zlabel('f(x, y)')
+
+    mesh_XYZ = {
+        "X": matplot_mesh_X,
+        "Y": matplot_mesh_Y,
+        "Z": Z
+    }
+
+    return axes, mesh_XYZ
+
+
+# 描画
+def play_plot(axes, mesh_XYZ, positions, personal_best_positions, personal_best_scores, global_best_particle_position, velocities):
+    axes.plot_wireframe(mesh_XYZ['X'], mesh_XYZ['Y'], mesh_XYZ['Z'], alpha=0.3)
+    axes.scatter(positions[:, 0], positions[:, 1], np.apply_along_axis(objective_function, 1, positions), marker='^',
+                 c="red", linewidths=5)
+    axes.scatter(personal_best_positions[:, 0], personal_best_positions[:, 1], personal_best_scores, linewidths=5,
+                 marker='x', c='blue')
+    axes.scatter(global_best_particle_position[0], global_best_particle_position[1],
+                 objective_function(global_best_particle_position), linewidths=8, marker='o', c='green')
+
+    axes.quiver(positions[:,0], positions[:,1], np.apply_along_axis(objective_function, 1, positions), velocities[:, 0], velocities[:, 1], np.zeros(len(velocities)), color='gray')
+    plt.draw()
+    plt.pause(1)
+    plt.cla()
 
 
 # 各粒子の位置更新
@@ -45,27 +94,8 @@ def main():
 
     xy_min, xy_max = -32, 32
 
-    matplot_x = np.arange(xy_min, xy_max, 1.0)
-    matplot_y = np.arange(xy_min, xy_max, 1.0)
-
-    matplot_mesh_X, matplot_mesh_Y = np.meshgrid(matplot_x, matplot_y)
-
-    Z = []
-    for i in range(len(matplot_mesh_X)):
-        z = []
-        for j in range(len(matplot_mesh_X[0])):
-            result = objective_function(np.array([matplot_mesh_X[i][j], matplot_mesh_Y[i][j]]))
-            z.append(result)
-        Z.append(z)
-    Z = np.array(Z)
-
-    plt.ion()
-    fig = plt.figure()
-    ax = Axes3D(fig)
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('f(x, y)')
+    # グラフの初期化
+    axes, mesh_XYZ = init_plot(xy_min, xy_max)
 
     # 各粒子の位置
     positions = np.array(
@@ -80,7 +110,7 @@ def main():
     # 各粒子ごとのパーソナルベストの値
     personal_best_scores = np.apply_along_axis(objective_function, 1, personal_best_positions)
 
-    # グローバルベストの粒子ID≤
+    # グローバルベストの粒子ID
     global_best_particle_id = np.argmin(personal_best_scores)
 
     # グローバルベスト位置
@@ -106,12 +136,9 @@ def main():
         global_best_particle_id = np.argmin(personal_best_scores)
         global_best_particle_position = personal_best_positions[global_best_particle_id]
 
-        # print(T, global_best_particle_position, objective_function(global_best_particle_position))
-        ax.plot_wireframe(matplot_mesh_X, matplot_mesh_Y, Z, alpha=0.3)
-        ax.scatter(positions[:, 0], positions[:, 1], personal_best_scores, marker='^', c="red", linewidths=5)
-        plt.draw()
-        plt.pause(1)
-        plt.cla()
+        # グラフ描画
+        play_plot(axes, mesh_XYZ, positions, personal_best_positions, personal_best_scores,
+                  global_best_particle_position, velocities)
 
 
 if __name__ == '__main__':
